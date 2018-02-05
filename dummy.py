7ca9b19254fda9c819fd1d9be279f2097572aaaf
Milo Hemken <mhemken@support.ucla.edu>
2018-02-05 12:50:06 -0800

    initial commit

---
+++ dummy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+class DummyClass:
+    '''For demonstration purposes only'''
+    
+    def __init__(self):
+        self.name = 'dummhy'
+        
+    def do_stuff(self):
+        return 'fee fi foe fum'
